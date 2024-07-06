84f8e03d4afded1262f339a85e5158f18012e3d8
Nguyễn Quang Sáng <76580837+quangsangmmo@users.noreply.github.com>
2024-07-06 18:42:22 +0700

    Update setting.py

--- anubistool/setting.py
+++ anubistool/setting.py
@@ -14,30 +14,3 @@ v_yescoin = "5.0.0"
 date_dailycombo = "05/07/2024/19/00/00"
 morse_hamster = "STAKE"
 list_combo = ["ux_ui_team","cx_hub_istanbul"]
-
-def getIP(proxies):
-  ip = fakeip = "None"
-  url_ip = ["https://api.ipify.org", "https://ipinfo.io/ip", "https://ipv4.icanhazip.com/", "https://api.ipgeolocation.io/getip"]
-  error_count = 0
-  max_errors = 10
-  while (ip == fakeip == "None" or ip == fakeip == " " or fakeip == ip == "") and error_count < max_errors:
-    try:
-      urlGetIP = random.choice(url_ip)
-      ip = requests.get(urlGetIP, timeout=30)
-      fakeip = requests.get(urlGetIP, proxies=proxies, timeout=30)
-    except Exception as e:
-      now_time = datetime.now().strftime("%H:%M:%S")
-      print(f"{đỏ}⟨{vàng}{now_time}{đỏ}⟩ Lấy IP thất bại: {e}\n")
-      ip = fakeip = "None"
-      error_count += 1
-      COUNTDOWN(10)
-  if error_count >= max_errors:
-    print("Quá số lần thử lại. Thoát.")
-    return None, None
-  try:
-    ip = ip.json()["ip"]
-    fakeip = fakeip.json()["ip"]
-  except:
-    ip = ip.text.strip()
-    fakeip = fakeip.text.strip()
-  return ip, fakeip
